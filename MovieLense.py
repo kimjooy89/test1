3e456596b60445f370436b9258eb3bca194b65b2
kimjooy89 <kimjooy89@gmail.com>
2020-04-13 23:12:34 +0900

    Update MovieLense.py

--- MovieLense.py
+++ MovieLense.py
@@ -1,25 +1,37 @@
-1) 사용자별 평균평점을 산출하여 제일 높은 사용자와, 제일 낮은 사용자 id 출력
+import numpy as np
+import pandas as pd
 
-[출력예시]
 
-1. 최고평점 : 사용자ID (복수일 경우 사용자ID1, 사용자ID2, .....)
+movies = pd.read_csv("./ml-latest-small/movies.csv")
+ratings = pd.read_csv("./ml-latest-small/ratings.csv")
 
-1. 최저평점 : 사용자ID (복수일 경우 사용자ID1, 사용자ID2, .....)
 
- 
+#1) 사용자별 평균평점을 산출하여 제일 높은 사용자와, 제일 낮은 사용자 id 출력
+user_rate = ratings.groupby('userId').mean()
 
-2. 영화별 평균평점을 산출하여 제일 높은 영화와 제일 낮은 영화의 제목을 출력
+user_rate_max = user_rate[user_rate['rating'] == user_rate['rating'].max()].index.tolist()
+user_rate_min = user_rate[user_rate['rating'] == user_rate['rating'].min()].index.tolist()
 
-[출력예시]
+print("1.최고평점 : " + user_rate_max.__str__())
+print("1.최저평점 : " + user_rate_min.__str__())
 
-2. 최고평점 : Dracula: Dead and Loving It (1995)  (복수일 경우 , 로 이어서 출력)
+#2. 영화별 평균평점을 산출하여 제일 높은 영화와 제일 낮은 영화의 제목을 출력
+print()
+movie_rate = ratings.groupby('movieId', as_index=False).mean()
+movie_rate_max = movie_rate['rating'].max()
+movie_rate_min = movie_rate['rating'].min()
 
-2. 최저평점 : Now and Then (1995) (복수일 경우 , 로 이어서 출력)
+#print(movie_rate_max)
+#print(movie_rate_min)
+#print(movie_rate)
 
- 
+merge = pd.merge(movie_rate, movies)
+print("2.최고평점 : " + merge[merge['rating'] == movie_rate_max]['title'].values)
+print("2.최저평점 : " + merge[merge['rating'] == movie_rate_min]['title'].values)
 
-3. 범죄스릴러(Crime, Thriller) 장르에서 최고 평점을 얻은 영화의 제목을 출력 :  
 
-[출력예시]
+#3. 범죄스릴러(Crime, Thriller) 장르에서 최고 평점을 얻은 영화의 제목을 출력 :
 
-3. 범죄스릴러 장르 최고평점 : Kiss of Death (1995)
+genre_movie = movies[movies["genres"].str.contains("Crime") | movies["genres"].str.contains("Thriiler")]
+genre_movie_rate = pd.merge(genre_movie, movie_rate)
+print("3.범죄스릴러 장르 최고평점 : " + genre_movie_rate[genre_movie_rate['rating'] == genre_movie_rate['rating'].max()]['title'].values)
